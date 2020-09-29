c32867198e6e1233813bc994c66fdf2b948124e0
Matvey9 <matvey.mog@gmail.com>
2020-09-29 11:13:52 +0300

    clone Archakov06

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export {default as Categories} from './Categories';
+export {default as Header} from './Header';
+export {default as SortPopup} from './SortPopup'
+export {default as PizzaBlock} from './PizzaBlock/index'
+export {default as PizzaLoadingBlock} from './PizzaBlock/LoadingBlock'
+export {default as CartItem} from './CartItem'
+
+
