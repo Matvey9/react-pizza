0ff3ed165a929a78c96fb12dd23d25ecdd71c4a9
Matvey9 <matvey.mog@gmail.com>
2020-09-29 11:45:43 +0300

    removed url from actions/pizza

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -12,9 +12,9 @@ export const fetchPizzas = (sortBy, category) => (dispatch) => {
     });
     axios
         .get(
-            `http://localhost:3000/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${
-                sortBy.type
-            }&_order=${sortBy.order}`,
+            `/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${
+                sortBy.order
+            }`,
         )
         .then(({ data }) => {
             dispatch(setPizzas(data));
